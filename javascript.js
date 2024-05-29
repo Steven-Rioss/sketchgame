e67c3b8fde0efd6c31e851ae65866e7b99db557b
Steven Rios <156548255+Steven-Rioss@users.noreply.github.com>
2024-05-29 19:15:10 -0400

    Create 16x16 grid of square divs

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// Function to create the grid
+function createGrid(rows, cols) {
+const gridContainer = document.querySelector('#gridContainer');
+for (let i = 0; i < rows * cols; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
+    gridContainer.appendChild(gridItem);
+    }
+}
+
+// Call the function to create a 16x16 grid
+createGrid(16, 16);
